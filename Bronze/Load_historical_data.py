a1604b7a6b88524740a859b9d53492ec99d46203
Lenovo <shraddhaamrutwar25@gmail.com>
2024-10-22 18:35:25 +0530

    Loaded Historical Data

---
+++ Bronze/Load_historical_data.py
@@ -0,0 +1,106 @@
+#################################################################################
+# Name : Load Historical Data By Pyspark.py
+# Description : Load the data after flattening
+# Date : 2024 - 10 - 22
+# Version : 3.3.2
+# Modified date :
+# Modification Description :
+#################################################################################
+
+from apache_beam.options.pipeline_options import PipelineOptions, GoogleCloudOptions, StandardOptions
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_unixtime, udf
+from pyspark.sql.functions import col, explode
+import json
+import logging
+from pyspark import SparkContext
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType, ArrayType, LongType, \
+    MapType, FloatType
+from utility import Utils
+from google.cloud import storage
+import apache_beam as beam
+import os
+
+if __name__ == '__main__':
+
+    os.environ[
+        'GOOGLE_APPLICATION_CREDENTIALS'] = r'C:\Users\Lenovo\Project_First\.venv\project-earthquake-439311-29bcc5e29e30.json'
+
+    # ================================================================================
+
+    # initialize the spark session
+    spark = SparkSession.builder.master("local[*]").appName("project").getOrCreate()
+
+    # ================================================================================
+
+    # Get the data from the API
+    url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_month.geojson'
+
+    utils_obj = Utils()
+
+    response = utils_obj.fetch_data_from_api(url)
+
+    print(type(response))
+
+    # ================================================================================
+
+    # upload file into GCS
+    upload_file = utils_obj.upload_to_gcs(response, 'earthquake_analysiss', 'pyspark/landing/20241022/Historical_data')
+
+    # read data from the GCS Bucket
+
+    # ================================================================================
+
+    read_data = utils_obj.read_file_from_gcs('earthquake_analysiss', 'pyspark/landing/20241022/Historical_data.json')
+    # print(read_data)
+
+    # ================================================================================
+
+    # flattening data
+    flattened_data = utils_obj.flatten_earthquake_data(read_data)
+
+    schema = utils_obj.schema()
+    #
+    # # Create DataFrame
+    df = spark.createDataFrame(flattened_data, schema=schema)
+    df.show()
+
+    # ================================================================================
+
+    # Columns like “time”, “updated” - convert its value from epoch to timestamp
+
+    updated_df = df.withColumn('time', from_unixtime(df['time'] / 1000)).withColumn('updated', from_unixtime(df['updated'] / 1000))
+
+    updated_df.show()
+
+    updated_df.printSchema()
+
+    # ================================================================================
+
+    # Generate column “area” - based on existing “place” column
+    #
+    def city(place):
+        if place:
+            words = place.split()
+            return words[-1]  # Return the last word
+            return None
+
+    ek_function = udf(city, StringType())
+
+    area_df = updated_df.withColumn('area', ek_function(col('place')))
+
+    area_df.show(truncate=False)
+
+    #write the df to GCS Bucket
+
+    json_data = df.toJSON().collect()  # json_data is a list of JSON strings
+
+    # upload_to_gcs1=utils_obj.upload_to_gcs(area_df,'earthquake_analysiss','silver/historical_data')
+    # print('Sucessfully Written')
+
+    data_to_upload = [json.loads(record) for record in json_data]
+
+    write_data=utils_obj.upload_to_gcs(data_to_upload,'earthquake_analysiss','silver/20241022/Historical_data')
+
+    print(f'File written successfully {write_data}')
+
---
+++ Bronze/utility.py
@@ -0,0 +1,133 @@
+import requests
+import json
+from google.cloud import storage
+import apache_beam as beam
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, DoubleType, ArrayType, LongType, MapType,FloatType
+from datetime import datetime
+
+
+class Utils:
+    def schema(self):
+        flattened_schema = StructType([
+        StructField("mag", DoubleType(), True),
+        StructField("place", StringType(), True),
+        StructField("time", LongType(), True),
+        StructField("updated", LongType(), True),
+        StructField("tz", IntegerType(), True),
+        StructField("url", StringType(), True),
+        StructField("detail", StringType(), True),
+        StructField("felt", IntegerType(), True),
+        StructField("cdi", FloatType(), True),
+        StructField("mmi", FloatType(), True),
+        StructField("alert", StringType(), True),
+        StructField("status", StringType(), True),
+        StructField("tsunami", IntegerType(), True),
+        StructField("sig", IntegerType(), True),
+        StructField("net", StringType(), True),
+        StructField("code", StringType(), True),
+        StructField("ids", StringType(), True),
+        StructField("sources", StringType(), True),
+        StructField("types", StringType(), True),
+        StructField("nst", IntegerType(), True),
+        StructField("dmin", FloatType(), True),
+        StructField("rms", FloatType(), True),
+        StructField("gap", FloatType(), True),
+        StructField("magType", StringType(), True),
+        StructField("type", StringType(), True),
+        StructField("title", StringType(), True),
+        StructField("longitude", FloatType(), True),
+        StructField("latitude", FloatType(), True),
+        StructField("depth", FloatType(), True)
+    ])
+
+        return flattened_schema
+
+    def fetch_data_from_api(self, url):
+        try:
+            response = requests.get(url)
+            return response.json()
+        except requests.exceptions.RequestException as e:
+            print(f"An error occurred: {e}")
+
+    def flatten_earthquake_data(self,even_data):
+
+        flattened_data=[]
+
+        features=even_data.get('features',[])
+
+        for feature in features:
+            properties = feature["properties"]
+            geometry = feature["geometry"]
+            coordinates = geometry["coordinates"]
+
+            flat_event = {
+                "mag": float(properties.get("mag")) if properties.get("mag") is not None else None,
+                "place": properties.get("place"),
+                "time": properties.get("time"),
+                "updated": properties.get("updated"),
+                "tz": properties.get("tz"),
+                "url": properties.get("url"),
+                "detail": properties.get("detail"),
+                "felt": properties.get("felt"),
+                "cdi": float(properties.get("cdi")) if properties.get("cdi") is not None else None,
+                "mmi": float(properties.get("mmi")) if properties.get("mmi") is not None else None,
+                "alert": properties.get("alert"),
+                "status": properties.get("status"),
+                "tsunami": properties.get("tsunami"),
+                "sig": properties.get("sig"),
+                "net": properties.get("net"),
+                "code": properties.get("code"),
+                "ids": properties.get("ids"),
+                "sources": properties.get("sources"),
+                "types": properties.get("types"),
+                "nst": properties.get("nst"),
+                "dmin": float(properties.get("dmin")) if properties.get("dmin") is not None else None,
+                "rms": float(properties.get("rms")) if properties.get("rms") is not None else None,
+                "gap": float(properties.get("gap")) if properties.get("gap") is not None else None,
+                "magType": properties.get("magType"),
+                "type": properties.get("type"),
+                "title": properties.get("title"),
+                "longitude": coordinates[0],
+                "latitude": coordinates[1],
+                "depth": float(coordinates[2]) if coordinates[2] is not None else None
+            }
+
+            flattened_data.append(flat_event)
+
+        return flattened_data
+    def upload_to_gcs(self, data, bucket_name, blob_name):
+
+        client = storage.Client()
+
+        # Get the bucket and blob objects
+        bucket = client.bucket(bucket_name)
+        blob = bucket.blob(blob_name)
+
+        # Convert the dict to JSON string for uploading
+        json_data = json.dumps(data)
+
+        # Upload the content directly from the response
+        blob.upload_from_string(json_data, content_type='application/json')
+
+        return print('successfully uploaded')
+
+    #Read the file from the GCS Bucket
+    def read_file_from_gcs(self,bucket_name,blob_name):
+
+        client = storage.Client()
+
+        bucket=client.bucket(bucket_name)
+
+        blob=bucket.blob(blob_name)
+
+        json_data = blob.download_as_text()
+
+        data = json.loads(json_data)  # Parse JSON into a Python dictionary
+
+        return data
+
+
+class FetchDataFromAPI(beam.DoFn):
+    def process(self, url):
+        response = requests.get(url)
+        return response.json()
